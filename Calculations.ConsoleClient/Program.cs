11185d140fa076a992b8eef08e64c8580cba8452
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.gitlab.com>
2023-08-01 17:29:14 +0300

    Fix .editorconfig, stylecop.json files.

--- Calculations.ConsoleClient/Program.cs
+++ Calculations.ConsoleClient/Program.cs
@@ -1,24 +1,19 @@
-﻿using System;
-using System.Threading.Tasks;
-
-namespace Calculation.ConsoleClient
+namespace Calculations.ConsoleClient;
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    /// Calculates the sum from 1 to n synchronously.
+    /// The value of n is set by the user from the console.
+    /// The user can change the boundary n during the calculation, which causes the calculation to be restarted,
+    /// this should not crash the application.
+    /// After receiving the result, be able to continue calculations without leaving the console.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public static async Task Main()
     {
-        /// <summary>
-        /// Calculates the sum from 1 to n synchronously.
-        /// The value of n is set by the user from the console.
-        /// The user can change the boundary n during the calculation, which causes the calculation to be restarted,
-        /// this should not crash the application.
-        /// After receiving the result, be able to continue calculations without leaving the console.
-        /// </summary>
-        /// <returns>A task that represents the asynchronous operation.</returns>
-        public static async Task Main()
+        while (true)
         {
-            while (true)
-            {
-                throw new NotImplementedException();
-            }
+            throw new NotImplementedException();
         }
     }
 }
--- Calculations/Calculator.cs
+++ Calculations/Calculator.cs
@@ -1,36 +1,31 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+namespace Calculations;
 
-namespace Calculations
+/// <summary>
+/// Presents methods for the calculation of the sum.
+/// </summary>
+public static class Calculator
 {
     /// <summary>
-    /// Presents methods for the calculation of the sum.
+    /// Calculates the sum from 1 to n synchronously.
     /// </summary>
-    public static class Calculator
+    /// <param name="n">The last number in the sum.</param>
+    /// <returns>A sum: 1 + 2 + ... + n.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Throw if n less or equals zero.</exception>
+    public static long CalculateSum(int n)
     {
-        /// <summary>
-        /// Calculates the sum from 1 to n synchronously.
-        /// </summary>
-        /// <param name="n">The last number in the sum.</param>
-        /// <returns>A sum: 1 + 2 + ... + n.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">Throw if n less or equals zero.</exception>
-        public static long CalculateSum(int n)
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
+    }
 
-        /// <summary>
-        /// Calculates the sum from 1 to n asynchronously.
-        /// </summary>
-        /// <param name="n">The last number in the sum.</param>
-        /// <param name="token">The cancellation token for the cancellation of the asynchronous operation.</param>
-        /// <param name="progress">Presents current status of the asynchronous operation in form of the current value of sum and index.</param>
-        /// <returns>A task that represents the asynchronous sum: 1 + 2 + ... + n.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">Throw if n less or equals zero.</exception>
-        public static Task<long> CalculateSumAsync(int n, CancellationToken token, IProgress<(int, long)>? progress = null)
-        {
-            throw new NotImplementedException();
-        }
+    /// <summary>
+    /// Calculates the sum from 1 to n asynchronously.
+    /// </summary>
+    /// <param name="n">The last number in the sum.</param>
+    /// <param name="token">The cancellation token for the cancellation of the asynchronous operation.</param>
+    /// <param name="progress">Presents current status of the asynchronous operation in form of the current value of sum and index.</param>
+    /// <returns>A task that represents the asynchronous sum: 1 + 2 + ... + n.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Throw if n less or equals zero.</exception>
+    public static Task<long> CalculateSumAsync(int n, CancellationToken token, IProgress<(int, long)>? progress = null)
+    {
+        throw new NotImplementedException();
     }
 }
